9aeb3823d1e39b676fcd423e6746ee965ca200fc
Rohan Chedde <129337865+rohanchedde@users.noreply.github.com>
2025-08-26 13:05:02 -0400

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -24,3 +24,32 @@ DELETE FROM cd.bookings;
 -- Question 6: Delete a member from the cd.members table
 DELETE FROM cd.members
 WHERE memid = 37;
+
+-- Question 7: Control which rows are retrieved
+SELECT facid, name, membercost, monthlymaintenance 
+    FROM cd.facilities 
+    WHERE (membercost < (monthlymaintenance * 0.02)) 
+    AND (membercost != 0);
+
+-- Question 8: Basic String Services
+SELECT * FROM cd.facilities WHERE name LIKE '%Tennis%';
+
+-- Question 9: Matching against multiple possible values
+SELECT * FROM cd.facilities WHERE facid IN (1,5);
+
+-- Question 10: Working with dates
+SELECT memid, surname, firstname, joindate FROM cd.members WHERE joindate > '2012-09-01';
+
+-- Question 11: Combining results from multiple queries
+SELECT surname FROM cd.members
+UNION
+SELECT name FROM cd.facilities;
+
+-- Question 12: Retrieve the start times of members' bookings
+SELECT bks.starttime 
+    FROM cd.bookings bks 
+        INNER JOIN cd.members mems
+            ON mems.memid = bks.memid
+    WHERE
+        mems.firstname='David'
+        AND mems.surname='Farrell';
